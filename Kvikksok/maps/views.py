fde565baa298fc952d4b99764d8ffdae5a8d20af
sarun <sarunj@stud.ntnu.no>
2021-02-16 20:40:34 +0100

    added mapbox to maps

--- Kvikksok/maps/views.py
+++ Kvikksok/maps/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'maps/home.html')
+    mapbox_access_token = 'pk.eyJ1Ijoic2t1cDI1MDYiLCJhIjoiY2trMnJidzJkMTNyaDJvdDdrMmpuODR1biJ9.UozLDX9kk8-CC4irjB1nNQ'
+    return render(request, 'maps\home.html', 
+                  { 'mapbox_access_token': mapbox_access_token })
 
 def about(request):
     return render(request, 'maps/about.html', {'title': 'About Us'})
