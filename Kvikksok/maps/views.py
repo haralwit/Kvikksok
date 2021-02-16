23508a39b4152ff2d23e18635c42f7d66544e27f
haralwit <haralwit@gmail.com>
2021-02-16 12:14:37 +0100

    add base.html and main.css

--- Kvikksok/maps/views.py
+++ Kvikksok/maps/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'maps/home.html')
 
 def about(request):
-    return render(request, 'maps/about.html')
+    return render(request, 'maps/about.html', {'title': 'About Us'})
 
