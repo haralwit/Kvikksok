96fdafa5be187c64aa4f715e21987c56a7349ced
haralwit <haralwit@gmail.com>
2021-02-16 11:08:08 +0100

    add generic about page

--- Kvikksok/maps/views.py
+++ Kvikksok/maps/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1> Hello world bitches </h1>")
+
+def about(request):
+    return HttpResponse('<h1> This is a generic about page </h1>')
+
