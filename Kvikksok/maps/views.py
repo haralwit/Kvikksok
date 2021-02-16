72de7b7b5d91f603b9a12ea8602ed0a6a2236fb9
haralwit <haralwit@gmail.com>
2021-02-16 11:25:07 +0100

    add maps html templates

--- Kvikksok/maps/views.py
+++ Kvikksok/maps/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1> Hello world bitches </h1>")
+    return render(request, 'maps/home.html')
 
 def about(request):
     return HttpResponse('<h1> This is a generic about page </h1>')
