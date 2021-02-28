a12b15c415537ddb63568a285b1c796b3bdd268f
sarun <sarunj@stud.ntnu.no>
2021-02-28 15:48:09 +0100

    lagt til knapp for å legge til 'marker'. Ved å trykke på søk får vi opp noen statiske postnummer punkter i Oslo.

--- Kvikksok/maps/urls.py
+++ Kvikksok/maps/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.home, name="maps-home"),
+    path('', views.home, name="maps-home"),
     path('about/', views.about, name="maps-about"),
     path('messages/', views.messages, name="maps-messages")
 ]
