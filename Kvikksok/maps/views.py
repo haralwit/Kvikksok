f48524547175d71e6be0c7eeb197aa4e65469fd0
Harald Witsø <haralwit@stud.ntnu.no>
2021-03-14 14:14:07 +0100

    add geojson serializer

--- Kvikksok/maps/views.py
+++ Kvikksok/maps/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
 from .models import Post, Postnummer
-
 
 def home(request):
     mapbox_access_token = 'pk.eyJ1Ijoic2t1cDI1MDYiLCJhIjoiY2trMnJidzJkMTNyaDJvdDdrMmpuODR1biJ9.UozLDX9kk8-CC4irjB1nNQ'
+    seralized_postnummer = serialize('geojson', Postnummer.objects.all())
+    context = {
+        'mapbox_access_token': mapbox_access_token,
+        'postnummers': seralized_postnummer,
+    }
     return render(request, 'maps\home.html', 
-                  { 'mapbox_access_token': mapbox_access_token,
-                  'postnummers': Postnummer.objects.values("geom").all()})
+                  context)
 
 def about(request):
     return render(request, 'maps/about.html', {'title': 'About Us'})
