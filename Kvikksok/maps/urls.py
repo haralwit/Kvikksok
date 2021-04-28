422cc61f22ec9a1bf0caae59a95247a06ee633c2
Harald Witsø <haralwit@stud.ntnu.no>
2021-04-29 00:53:30 +0200

    add delete view

--- Kvikksok/maps/urls.py
+++ Kvikksok/maps/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('messages/<int:pk>/', views.PostDetailView.as_view(), name="maps-messages-detail"),
     path('messages/new/', views.PostCreateView.as_view(), name="maps-messages-create"),
     path('messages/<int:pk>/update/', views.PostUpdateView.as_view(), name="maps-messages-update"),
+    path('messages/<int:pk>/delete/', views.PostDeleteView.as_view(), name="maps-messages-delete"),
     path('addMarker/', views.savePost, name='maps-addMarker')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
